9e14ef1541ae738eb2d7f60ae04820325bd5874f
Peter Bryant <pbryant@peterbryant.me.uk>
2023-02-15 19:46:59 +0000

    feat(init): convert docs to beach-safe

--- apps/beach-safe/pages/index.tsx
+++ apps/beach-safe/pages/index.tsx
@@ -1,9 +1,9 @@
 import { Button } from "ui";
 
-export default function Docs() {
+export default function BeachSafe() {
   return (
     <div>
-      <h1>Docs</h1>
+      <h1>Beach Safe</h1>
       <Button />
     </div>
   );
